794de2bf5fd74e652be40b759d091004b476d0e1
Kung-Hsiang <a86792511@gmail.com>
2020-01-25 02:58:32 +0800

    add the code to plot N2/N2ddt vs rho plots

--- N2_study.C
+++ N2_study.C
@@ -23,11 +23,11 @@
 #define Nrho 16
 #define Minrho -5
 #define Maxrho -1
-// pt bin region [200, 350, 480,1000]
+// pt bin region [200, 400, 600, 800]
 #define pt_r1 200
-#define pt_r2 350
-#define pt_r3 480
-#define pt_r4 1000
+#define pt_r2 400
+#define pt_r3 600
+#define pt_r4 800
 #define Npt 3
 using namespace std;
 
@@ -121,34 +121,46 @@ void Draw_and_write( vector<vector<Data>>& v, string name, vector<Double_t>& vn2
 	}
 
 	// plot 
+	gStyle->SetOptStat("");	
+	//TLegend* legend = new TLegend(0.7,0.7,0.9,0.9);
+	//legend->AddEntry(dt1,"X(20% QCD selection eff.)");
 	auto c1 = new TCanvas(Form("c_%s_n2b1",name.c_str()),Form("c_%s_n2b1",name.c_str()) );
+	h1->SetTitle(Form("%s_n2b1",name.c_str()));
 	h1->Project3D("XY")->Draw("VIOLIN(03001000)");
 	dt1->Draw("* SAME");
+	//legend->Draw();
 	c1->Write();
 	c1->SaveAs(Form("c_%s_n2b1.png",name.c_str()) );
 	auto c2 = new TCanvas(Form("c_%s_n2b2",name.c_str()),Form("c_%s_n2b2",name.c_str()) );
+	h2->SetTitle(Form("%s_n2b2",name.c_str()));
 	h2->Project3D("XY")->Draw("VIOLIN(03001000)");
-	dt2->Draw(	"* SAME");
+	dt2->Draw("* SAME");
+	//legend->Draw();
 	c2->Write();
 	c2->SaveAs(Form("c_%s_n2b2.png",name.c_str()) );
 	auto c3 = new TCanvas(Form("c_%s_n2b1DDT",name.c_str()),Form("c_%s_n2b1DDT",name.c_str()) );
+	h3->SetTitle(Form("%s_n2b1DDT",name.c_str()));
 	h3->Project3D("XY")->Draw("VIOLIN(03001000)");
 	dt0->Draw("* SAME");
+	//legend->Draw();
 	c3->Write();
 	c3->SaveAs(Form("c_%s_n2b1DDT.png",name.c_str()) );
 	auto c4 = new TCanvas(Form("c_%s_n2b2DDT",name.c_str()),Form("c_%s_n2b2DDT",name.c_str()) );
+	h4->SetTitle(Form("%s_n2b2DDT",name.c_str()));
 	h4->Project3D("XY")->Draw("VIOLINX(03001000)");
 	dt0->Draw("* SAME");
+	//legend->Draw();
 	c4->Write();
 	c4->SaveAs(Form("c_%s_n2b2DDT.png",name.c_str()) );
 }
 
 void N2_study(){
 	vector< vector<Data>> v1(Nrho); // used to store & sort the data in different rho region
-	vector< vector<Data>> v_pt1(Nrho),v_pt2(Nrho),v_pt3(Nrho); // used to store & sort the data in different pt region
+	vector< vector<Data>> v_pt1(Nrho),v_pt2(Nrho),v_pt3(Nrho),v_pt4(Nrho); // used to store & sort the data in different pt region
 	vector< Data > v_total;
 	vector< Double_t > n2b1_v20, n2b1_v26, n2b1_v5, n2b1_v50, n2b1_cut;
 	vector< Double_t > n2b2_v20, n2b2_v26, n2b2_v5, n2b2_v50;
+	vector< Double_t > v_for_no_used,v_for_no_used_2;
 	TH3D* h_n2b1 = new TH3D("h_n2b1","h_n2b1",NN2,MinN2,MaxN2, Nrho,Minrho,Maxrho, 3,0, Maxpt); // for short test
 	TH3D* h_n2b2 = (TH3D*) h_n2b1->Clone("h_n2b2");
 	// input file data
@@ -183,28 +195,37 @@ void N2_study(){
 			if (*pt >= pt_r1 && *pt < pt_r2 ) v_pt1[region_rho].push_back(Data(*n2b1,*n2b2,*rho,*pt));
 			else if (*pt >= pt_r2 && *pt < pt_r3) v_pt2[region_rho].push_back(Data(*n2b1,*n2b2,*rho,*pt));
 			else if (*pt >= pt_r3 && *pt < pt_r4) v_pt3[region_rho].push_back(Data(*n2b1,*n2b2,*rho,*pt));
-		
-			if (*pt>Maxpt ) overN++;
-			else if (*pt<0) lowerN++;
+			else if (*pt >= pt_r4) v_pt4[region_rho].push_back(Data(*n2b1,*n2b2,*rho,*pt));
+			if (*pt<0) lowerN++;
 		}
 	}
 
 	// prepare to output
 	TFile* fout = new TFile("TH3_output.root","NEW");
 	TTree outTree("tree","out branches");
+	outTree.Branch("n2b1_v5", &n2b1_v5);
+	outTree.Branch("n2b2_v5", &n2b2_v5);
 	outTree.Branch("n2b1_v20", &n2b1_v20);
 	outTree.Branch("n2b2_v20", &n2b2_v20);
 	outTree.Branch("n2b1_v26", &n2b1_v26);
 	outTree.Branch("n2b2_v26", &n2b2_v26);
-	outTree.Branch("n2b1_v5", &n2b1_v5);
-	outTree.Branch("n2b2_v5", &n2b2_v5);
 	outTree.Branch("n2b1_v50", &n2b1_v50);
 	outTree.Branch("n2b2_v50", &n2b2_v50);
 	outTree.Branch("n2b1_cut", &n2b1_cut);
+	
+	
 	Draw_and_write(v1,"20",n2b1_v20,n2b2_v20,0.2);
 	Draw_and_write(v1,"26",n2b1_v26,n2b2_v26,0.26);
 	Draw_and_write(v1,"5",n2b1_v5,n2b2_v5,0.05);
 	Draw_and_write(v1,"50",n2b1_v50,n2b2_v50,0.5);
+	
+	Draw_and_write(v_pt1,"pt200to400",v_for_no_used,v_for_no_used_2,0.2);
+	v_for_no_used.clear(); v_for_no_used_2.clear();
+	Draw_and_write(v_pt2,"pt400to600",v_for_no_used,v_for_no_used_2,0.2);
+	v_for_no_used.clear(); v_for_no_used_2.clear();
+	Draw_and_write(v_pt3,"pt600to800",v_for_no_used,v_for_no_used_2,0.2);
+	v_for_no_used.clear(); v_for_no_used_2.clear();
+	Draw_and_write(v_pt4,"ptOver800",v_for_no_used,v_for_no_used_2,0.2);
 	
 	// get different N2b1 cut
 	sort_Nper(v_total,n2b1_cut);
@@ -228,6 +249,6 @@ void N2_study(){
 	
 	outTree.Fill();
 	fout->Write();
-	cout << "lower: " << lowerN << "| over: " << overN << endl;
+	cout << "lower: " << lowerN << endl;
 	fout->Close();
 }
