cb9356d9eb83a47c99eccf731f4ad2d3fa4bb9c2
Kung-Hsiang <a86792511@gmail.com>
2020-03-13 16:02:19 +0800

    for preparing the datacard

---
+++ SF/makemyDatacard.C
@@ -0,0 +1,49 @@
+/*
+
+captp1 = N2DDT(26%)
+
+*/
+
+
+using namespace std;
+
+void makemyDatacard(string data, string MC) {
+
+  TFile *fdata = TFile::Open((TString)data,"READONLY");
+  TFile *fMC = TFile::Open((TString)MC,"READONLY");
+  TH1D  *h_obs_pass = (TH1D*)fdata->Get("h_pass");
+  TH1D  *h_obs_fail = (TH1D*)fdata->Get("h_fail");
+  TH1D  *h_catp1_pass = (TH1D*)fMC->Get("h_pass");
+  TH1D  *h_catp1_fail = (TH1D*)fMC->Get("h_fail");
+  ofstream myfile("SF.txt");
+  
+  
+  myfile << "imax 2  number of channels\n";
+  myfile << "jmax 1  number of backgrounds\n";
+  myfile << "kmax *  number of nuisance parameters (sources of systematical uncertainties)\n";
+  myfile << "------------\n";
+  myfile << "shapes  *  pass   " << data << "  $PROCESS $PROCESS_$SYSTEMATIC\n";
+  myfile << "shapes  *  fail   " << MC << "  $PROCESS $PROCESS_$SYSTEMATIC\n";
+  myfile << "------------\n";
+  myfile << "# we have just one channel, in which we observe 0 events\n";
+  myfile << "bin             pass    fail\n";
+  myfile << "observation      " << h_obs_pass->Integral(1,h_obs_pass->GetNbinsX()) << "  " <<  h_obs_fail->Integral(1,h_obs_fail->GetNbinsX())  << "\n";
+  myfile << "------------\n";
+  myfile << "# now we list the expected events for signal and all backgrounds in that bin\n";
+  myfile << "# the second 'process' line must have a positive number for backgrounds, and 0 for signal\n";
+  myfile << "# then we list the independent sources of uncertainties, and give their effect (syst. error)\n";
+  myfile << "# on each process and bin\n";
+  myfile << "bin               pass         fail\n";
+  myfile << "process           catp1        catp1\n";  
+  myfile << "process            1            1\n";
+  myfile << "rate            " 
+	    <<  h_catp1_pass->Integral(1,h_catp1_pass->GetNbinsX()) << " "
+	    <<  h_catp1_fail->Integral(1,h_catp1_fail->GetNbinsX()) << "\n";
+  myfile << "------------\n";
+  myfile << "lumi    lnN    1.023  1.023  1.023  1.023\n";
+  myfile << "jecs    lnN    1.02   1.02   1.02   1.02 \n";
+  myfile << "pu      lnN    1.05   1.05   1.05   1.05 \n";
+
+  myfile << "*  autoMCStats  0\n";
+  
+}
---
+++ SF/prepare_root.C
@@ -0,0 +1,74 @@
+/*
+	this is used to prepare the TH1 in root file for the datacard input
+	we have 4 pt regions for the cut [200-400],[400-600],[600-800],[>800]
+*/
+#include <iostream>
+#include <sstream>
+#include <TTree.h>
+#include <TFile.h>
+#include <TH3D.h>
+#include <TH2D.h>
+#include <TROOT.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TLatex.h>
+#include <TAxis.h>
+// the def. in cut histo.
+#define pt_r1 200
+#define pt_r2 400
+#define pt_r3 600
+#define pt_r4 800
+
+#define NN2 28
+#define MinN2 -0.2
+#define MaxN2 0.5
+
+#define Nrho 20
+#define Minrho -6
+#define Maxrho -1
+double d = (double)(Maxrho-Minrho)/(double)Nrho; // the width in rho histo.
+string cut_branch = "N2b1_v26";  //used to read cut root file branch
+
+using namespace std;
+void prepare_root(string inputname,string outputname="tmp.root", string cutname="TH3_output.root"){
+	TFile* fcut = new TFile((TString)cutname,"READ");
+	TH2D* h_cut = (TH2D*) fcut->Get("h_pt_rho");
+
+	TFile* fin = new TFile((TString)inputname,"READ");
+	TTreeReader myRead("monoHbb_SR_boosted",fin);
+	TTreeReaderValue< Double_t > N2(myRead,"FJetN2b1");
+	TTreeReaderValue< Double_t > pt(myRead,"FJetPt");
+	TTreeReaderValue< Double_t > rho(myRead,"FJetrho");
+	
+	TFile* fout = new TFile((TString)outputname,"NEW");
+	TH1D* h_pass = new TH1D("h_pass","h_pass",NN2,MinN2,MaxN2);
+	TH1D* h_fail = (TH1D*) h_pass->Clone("h_fail"); h_fail->SetTitle("h_fail");
+	int N = myRead.GetEntries();
+	int x,y;
+	int i=0;
+	while(myRead.Next()){
+		// for making fake data
+		i++;
+		if (i > N/2) continue;
+		// deside in which pt region
+		if (*pt >= pt_r1 && *pt < pt_r2 ) y = 1;
+		else if (*pt >= pt_r2 && *pt < pt_r3) y = 2;
+		else if (*pt >= pt_r3 && *pt < pt_r4) y = 3;
+		else if (*pt >= pt_r4) y = 4;
+		else continue;
+		// deside in which rho region
+		if (*rho < Minrho) continue;
+		x = (ceil)(*rho - Minrho) / d;
+		// deside pass or fail
+		double N2cut = h_cut->GetBinContent(x,y);
+		if (N2cut == 0) continue;
+		double N2ddt = *N2 - N2cut;
+		if(N2ddt<0) h_pass->Fill(N2ddt);
+		else h_fail->Fill(N2ddt);
+		
+	}
+
+	fout->Write();
+	fout->Close();
+	
+}
