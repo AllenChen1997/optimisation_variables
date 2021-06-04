97450013aa5573ad9932db65a12f2e0099f237b2
root <root@LAPTOP-5N5PN2IE.localdomain>
2021-06-04 13:28:20 +0800

    use to draw histograms from mini tree

---
+++ QCD_data_driven/DrawSRtest.C
@@ -0,0 +1,166 @@
+/*
+	this code is used to get r plots with different selections
+	the cuts: tauveto, photonveto, electron veto, muon veto, nAk4, nFatJ
+*/
+
+using namespace std;
+
+template<typename T>
+void setDrawOpt(T& h,string title, string xTitle, string yTitle){
+	h->SetTitle(title.c_str());
+	h->SetTitleSize(0.07);
+	h->GetXaxis()->SetLabelSize(0.05);
+	h->GetXaxis()->SetTitleSize(0.05);
+	h->GetYaxis()->SetLabelSize(0.05);
+	h->GetYaxis()->SetTitleSize(0.05);
+	h->SetXTitle(xTitle.c_str());
+	h->SetYTitle(yTitle.c_str());
+}
+
+void runCode(int n=6,bool isTest = false){
+	// outputs // 	
+	vector< TH1F* > h_met_mindphi_l(6); // the histo for mindphi > 0.4, 6 is for 6 path trigger regions
+	vector< TH1F* > h_met_mindphi_s(6); // the histo for mindphi < 0.4
+	vector< TH1F* > h_met_mindphi_l_(6); // _ means there is no SR cuts
+	vector< TH1F* > h_met_mindphi_s_(6);
+	vector< TH1F* > h_cutFlow(8); // cutFlows, 8 is the number of inputs
+	vector< TH1F* > tmp_l(6);
+	vector< TH1F* > tmp_s(6); 
+	vector< TH1F* > tmp_l_(6); 
+	vector< TH1F* > tmp_s_(6);
+	vector< int > tmp_weight; // total 2 element, 0: withSRcut , 1: no SRcut
+	string subline = "_withSR";
+	Double_t xbins[16] = {0, 25, 50, 75, 100, 125, 150, 175, 200, 250, 300, 350, 400, 500, 600, 1000};
+	string label[6] = {"tauVeto","photonVeto","EleVeto","LooseMuVeto","extraAk4","nFatJ"};
+
+	for (int i=0; i< h_met_mindphi_l.size(); i++){
+		h_met_mindphi_l[i] = new TH1F(Form("h_met_mindphi_l_withSR_%i",i),"",15,xbins);
+		h_met_mindphi_s[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("h_met_mindphi_s_withSR_%i",i));
+		h_met_mindphi_l_[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("h_met_mindphi_l_%i",i));
+		h_met_mindphi_s_[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("h_met_mindphi_s_%i",i));
+		tmp_l[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("tmp_l_withSR_%i",i));
+		tmp_s[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("tmp_s_withSR_%i",i));
+		tmp_l_[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("tmp_l_%i",i));
+		tmp_s_[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("tmp_s_%i",i));
+		
+		// call sumw2
+		h_met_mindphi_l[i]->Sumw2();
+		h_met_mindphi_s[i]->Sumw2();
+		h_met_mindphi_l_[i]->Sumw2();
+		h_met_mindphi_s_[i]->Sumw2();
+		tmp_l[i]->Sumw2();
+		tmp_s[i]->Sumw2();
+		tmp_l_[i]->Sumw2();
+		tmp_s_[i]->Sumw2();
+	}	
+	for (int i=0; i< h_cutFlow.size(); i++){
+		h_cutFlow[i] = new TH1F(Form("h_cutFlow_%i",i),"",6,0,6);
+	}
+	
+	// load files 
+	string inputlist[8] = {"QCD_100to200.root","QCD_200to300.root","QCD_300to500.root","QCD_500to700.root","QCD_700to1000.root","QCD_1000to1500.root","QCD_1500to2000.root","QCD_2000toInf.root"};
+	float xs[8] = {	23700000.0, 1547000.0, 322600.0, 29980.0, 6334.0, 1088.0, 99.11, 20.23};
+	for (int i=0; i<8; i++){
+		// reset all vectors 
+		for (int x=0; x< tmp_l.size(); x++){
+			tmp_l[x]->Reset("ICESM");
+			tmp_s[x]->Reset("ICESM");
+			tmp_l_[x]->Reset("ICESM");
+			tmp_s_[x]->Reset("ICESM");
+		}
+		tmp_weight = {0,0};
+		
+		cout << "reading " << inputlist[i] << endl;
+		TFile* fin = new TFile(inputlist[i].data(), "READONLY");
+		TTreeReader data("tree",fin);
+		TTreeReaderArray<bool> cuts(data,"cuts");
+		TTreeReaderValue<float> mindphi(data,"mindphi");
+		TTreeReaderValue<float> metpT(data,"metpT");
+		TTreeReaderValue<int> whichHT(data,"whichHT");
+		TTreeReaderValue<int> weight(data,"weight");
+		
+		int total_entry = data.GetEntries(true);
+		int jEntry= 0;
+		while(data.Next() ){
+			jEntry++;
+			if (isTest && jEntry == 100) break; 
+			if (jEntry % 1000000 == 0) cout <<  "Processing event " << jEntry << " of " << total_entry << endl;
+			else if (jEntry == total_entry) cout <<  "Processing event " << jEntry << " of " << total_entry << endl;
+			
+			// check cuts //
+			if (*whichHT < 0 ) continue;
+			if (jEntry%2 == 0) {
+				tmp_weight[1]++;
+				if (*mindphi > 0.4) tmp_l_[*whichHT]->Fill(*metpT);
+				else tmp_s_[*whichHT]->Fill(*metpT);
+			} else{
+				/*bool ispass = true;
+				for (int ncut = 0; ncut < n; ncut++){
+					if (! cuts[ncut]){
+						ispass = false;
+						break;
+					}
+					h_cutFlow[i]->Fill(ncut);
+				}
+				if (! ispass ) continue;*/
+				if (! cuts[n-1]) continue;
+				tmp_weight[0]++;
+				if (*mindphi > 0.4) tmp_l[*whichHT]->Fill(*metpT);
+				else tmp_s[*whichHT]->Fill(*metpT);
+			}
+			
+		} // end of entries
+		for (int k=0; k<h_met_mindphi_l.size(); k++){
+			h_met_mindphi_l[k]->Add(tmp_l[k], xs[i] /(float) tmp_weight[0]);
+			h_met_mindphi_s[k]->Add(tmp_s[k], xs[i] /(float) tmp_weight[0]);
+			h_met_mindphi_l_[k]->Add(tmp_l_[k], xs[i] /(float) tmp_weight[1]);
+			h_met_mindphi_s_[k]->Add(tmp_s_[k], xs[i] /(float) tmp_weight[1]);
+		}
+	} // end of all files
+		
+	gStyle->SetOptStat("");
+	TFile* fout = new TFile(Form("keep_histo_cut%i.root",n),"RECREATE");
+	TCanvas* c = new TCanvas("c","c");
+	c->Divide(1,2,0,0);
+	TH1F* htmp = (TH1F*) h_met_mindphi_l[0]->Clone("htmp");
+	TH1F* htmp2 = (TH1F*) h_met_mindphi_l_[0]->Clone("htmp2");
+	TH1F* htmp21 = (TH1F*) h_met_mindphi_l_[0]->Clone("htmp21");
+	for (int i = 0; i< h_met_mindphi_l.size() ;i++){
+		c->cd(1);
+		htmp->Divide(h_met_mindphi_l[i],h_met_mindphi_s[i] );
+		setDrawOpt(htmp,"","MET","");
+		htmp2->Divide(h_met_mindphi_l_[i],h_met_mindphi_s_[i] );
+		htmp->Draw("HIST E TEXT0");
+		htmp2->SetLineColor(kRed);
+		htmp2->Draw("HIST E SAME");
+		c->cd(2);
+		htmp21->Reset("ICESM");
+		htmp21->Add(htmp);
+		htmp21->Add(htmp2,-1);
+		setDrawOpt(htmp21,"","MET","");
+		htmp21->Draw("HIST E");
+		c->SaveAs(Form("rFunction_MC_cut%i_%i.png",n,i) );
+		
+		// save into root file
+		htmp->SetName(Form("h_withSR_%i",i) );
+		htmp2->SetName(Form("h_%i",i) );
+		htmp->Write();
+		htmp2->Write();
+		
+	}
+	fout->Close();
+	/*TCanvas* c2 = new TCanvas("c2","c2");
+	for (int i=0; i<h_cutFlow.size(); i++){
+		for (int j=0; j< (int)(sizeof(label)/sizeof(label[0]) ); j++){
+			h_cutFlow[i]->GetXaxis()->SetBinLabel(j+1,label[j].data() );
+		}
+		h_cutFlow[i]->Draw("HIST TEXT0");
+		c2->SaveAs(Form("cutFlow_%i.png",i));
+	}*/
+	
+}
+void DrawSRtest(){
+	for (int i=1; i<=6; i++){
+		runCode(i,false);
+	}
+}
