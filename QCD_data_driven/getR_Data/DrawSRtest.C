644452697c0e776ca32abfbd9836787670727caf
Kung-Hsiang Chen <a86792511@gmail.com>
2022-07-27 15:36:03 +0800

    update draw and fitting code for getR for signal

---
+++ QCD_data_driven/getR_Data/DrawSRtest.C
@@ -0,0 +1,188 @@
+/*
+	this code is used to get r plots after the cuts below
+	the cuts in cutFlowLabel
+	
+	in this version we have 8 MCs, 1 data
+	
+	there exist bugs for letting code work for both MC and data at the same time.
+	lets focus on data first
+	the data name and number of HT regions  need to adjust by manually
+
+*/
+
+using namespace std;
+
+
+template<typename T>
+void setDrawOpt(T& h,string title, string xTitle, string yTitle){
+	h->SetTitle(title.c_str());
+	h->SetTitleSize(0.07);
+	h->GetXaxis()->SetLabelSize(0.05);
+	h->GetXaxis()->SetTitleSize(0.05);
+	h->GetYaxis()->SetLabelSize(0.05);
+	h->GetYaxis()->SetTitleSize(0.05);
+	h->SetXTitle(xTitle.c_str());
+	h->SetYTitle(yTitle.c_str());
+}
+
+void DrawSRtest(bool isTest = false){
+	// load files 
+	
+	string inputDatalist = "JetHT-Run2017B-31Mar2018-v1.root";
+	string outputDataname = "JetHT-Run2017B-31Mar2018-v1";
+	int weight; // this one is used for initailization the "weight" variable for MC files(but there is no weight in data)
+	
+	int totalFiles = 1;
+	
+	// outputs // 	
+		// decide the number of output regions
+	int totalNRange = 0;
+	TFile* fin_0 = new TFile(inputDatalist.data(), "READONLY");
+	TH1F* h_HTRange = (TH1F*) fin_0->Get("h_HTRange");
+	totalNRange = h_HTRange->GetNbinsX() + 1;
+	
+	vector< TH1F* > h_met_mindphi_l(totalNRange); // the histo for mindphi > 0.4, 6 is for 6 path trigger regions
+	vector< TH1F* > h_met_mindphi_s(totalNRange); // the histo for mindphi < 0.4
+	vector< TH1F* > tmp_l(totalNRange); // used in each file, after loop all entries. append to h_met_mindphi_l
+	vector< TH1F* > tmp_s(totalNRange); 
+	vector< TH1F* > h_n2b1(totalNRange);  // this is used to collect the N2b1 plot in different HT regions
+	vector< TH1F* > tmp_n2b1(totalNRange); // this is used to get N2b1 plot in different HT regions in each file
+	
+	Double_t xbins[16] = {0, 25, 50, 75, 100, 125, 150, 175, 200, 250, 300, 350, 400, 500, 600, 1000};
+	string cutFlowLabel[11] = {"incl", "HTcut", "passTrig", "tauVeto", "photonVeto", "EleVeto", "LooseMuVeto", "extraAk4", "nFatJ", "hasN2B1", "atleast1AK4"};
+	TH2F* h_map_l = new TH2F("map_l","",15,xbins,6,0,6);
+	TH2F* h_map_s = (TH2F*) h_map_l->Clone("map_s");
+	
+	TH2F* h_DDB_N2 = new TH2F("DDB_N2","",20,0,0.5,20,0,1);
+	setDrawOpt(h_DDB_N2,"","N2B1","DDB");
+	TH2F* tmp_DDB_N2 = (TH2F*) h_DDB_N2->Clone("tmp_DDB_N2");
+
+	for (int i=0; i< h_met_mindphi_l.size(); i++){
+		h_met_mindphi_l[i] = new TH1F(Form("h_met_mindphi_l_withSR_%i",i),"",15,xbins);
+		h_met_mindphi_s[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("h_met_mindphi_s_withSR_%i",i));
+		tmp_l[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("tmp_l_withSR_%i",i));
+		tmp_s[i] = (TH1F*) h_met_mindphi_l[i]->Clone(Form("tmp_s_withSR_%i",i));
+		h_n2b1[i] = new TH1F(Form("h_n2b1_withHT_%i",i),"",20,0,0.5);
+		tmp_n2b1[i] = (TH1F*) h_n2b1[i]->Clone(Form("tmp_n2b1_withHT_%i",i));
+		// call sumw2
+		h_met_mindphi_l[i]->Sumw2();
+		h_met_mindphi_s[i]->Sumw2();
+		tmp_l[i]->Sumw2();
+		tmp_s[i]->Sumw2();
+	}	
+	
+	
+	for (int iFile=0; iFile<totalFiles; iFile++){
+		// reset all vectors 
+		for (int x=0; x< tmp_l.size(); x++){
+			tmp_l[x]->Reset("ICESM");
+			tmp_s[x]->Reset("ICESM");
+			tmp_n2b1[x]->Reset("ICESM");
+		}
+		h_map_l->Reset("ICESM");
+		h_map_s->Reset("ICESM");
+		tmp_DDB_N2->Reset("ICESM");
+		
+		TFile* fin;
+		cout << "reading " << inputDatalist << endl;
+		fin = new TFile(inputDatalist.data(), "READONLY");
+		
+		TTreeReader data("tree",fin);
+		TTreeReaderValue<float> mindphi(data,"mindphi");
+		TTreeReaderValue<float> metpT(data,"metpT");
+		TTreeReaderArray<float> DDB(data,"DDB");
+		TTreeReaderArray<float> N2B1(data,"N2B1");
+		TTreeReaderValue<int> whichHT(data,"whichHT");
+		TTreeReaderValue<int> weight(data,"weight");
+		TH1F* h_cutFlow = (TH1F*) fin->Get("h_cutFlow");
+		
+		int total_entry = data.GetEntries(true);
+		int jEntry= 0;
+		int nN2B1gt1 = 0;
+		while(data.Next() ){
+			// deal with the entries in the tree
+			jEntry++;
+			if (isTest && jEntry == 100) break; // in test mode we will have only 100 events
+			if (jEntry % 1000000 == 0) cout <<  "Processing event " << jEntry << " of " << total_entry << endl;
+			else if (jEntry == total_entry) cout <<  "Processing event " << jEntry << " of " << total_entry << endl;
+			
+			if (N2B1.GetSize() > 1) nN2B1gt1++;
+			
+
+			tmp_DDB_N2->Fill(N2B1[0],DDB[0]); // this is only applied the first 6 cuts
+			
+			if (*mindphi > 0.4) {
+				tmp_l[*whichHT]->Fill(*metpT,*weight);
+				tmp_n2b1[*whichHT]->Fill(N2B1[0],*weight);
+			} else {
+				tmp_s[*whichHT]->Fill(*metpT,*weight);
+			}
+			
+			
+		} // end of entries
+		cout << "nN2B1gt1= " << nN2B1gt1 << endl;
+
+		for (int k=0; k<h_met_mindphi_l.size(); k++){
+
+			h_met_mindphi_l[k]->Add(tmp_l[k]);
+			h_met_mindphi_s[k]->Add(tmp_s[k]);
+			h_n2b1[k]->Add(tmp_n2b1[k]);
+			
+			for (int nbin = 1; nbin<=15; nbin++){
+				h_map_l->SetBinContent(nbin,k+1,tmp_l[k]->GetBinContent(nbin) );
+				h_map_s->SetBinContent(nbin,k+1,tmp_s[k]->GetBinContent(nbin) );
+			}
+		}
+		h_DDB_N2->Add(tmp_DDB_N2);
+		
+		
+		gStyle->SetOptStat("");
+		string tmpOutputName;
+		tmpOutputName = outputDataname;	
+		// draw the cutflow plot
+		TCanvas* c2 = new TCanvas("c2","c2");
+		for (int ilabel=0; ilabel<sizeof(cutFlowLabel)/sizeof(cutFlowLabel[0]); ilabel++){
+			h_cutFlow->GetXaxis()->SetBinLabel(ilabel+1,cutFlowLabel[ilabel].data() );	
+		}
+		h_cutFlow->Draw();
+		c2->SaveAs(Form("cutFlow_%s.png",tmpOutputName.data() ) );
+		
+		// draw the map of mindphi larger than 0.4 or smaller 0.4
+		TCanvas* c3 = new TCanvas("c3","c3");
+		h_map_l->Draw("COLZ");
+		c3->SaveAs(Form("map_l_withSR_%s.png",tmpOutputName.data() ) );
+		h_map_s->Draw("COLZ");
+		c3->SaveAs(Form("map_s_withSR_%s.png",tmpOutputName.data() ) );
+	} // end of all files
+		
+
+	//TFile* fout = new TFile(Form("keep_histo_cut%i.root",n),"RECREATE");
+	TFile* fout = new TFile("keep_histo_cutFull.root","RECREATE");
+	TCanvas* c = new TCanvas("c","c");
+	TH1F* htmp = (TH1F*) h_met_mindphi_l[0]->Clone("htmp"); // to get ratio plot of mindphi ? over mindphi <
+
+	for (int i = 0; i< h_met_mindphi_l.size() ;i++){
+		
+		htmp->Divide(h_met_mindphi_l[i], h_met_mindphi_s[i] );
+		setDrawOpt(htmp,"","MET","");
+		htmp->Draw("HIST E TEXT0");
+
+		c->SaveAs(Form("rFunction_MC_%i.png",i) );
+		
+		// save into root file
+		htmp->SetName(Form("h_withSR_%i",i) );
+		htmp->Write();
+
+		h_n2b1[i]->Write();
+	}
+	fout->Close();
+	
+
+	
+		// draw DDB_ N2 plot 
+	TCanvas* c3 = new TCanvas("c3","c3");
+	h_DDB_N2->Draw("CANDLE");
+	c3->SaveAs("DDB_N2.png");
+
+	
+}
---
+++ QCD_data_driven/getR_Data/fit_from_MC.C
@@ -0,0 +1,216 @@
+/* 
+	this code is used to study N2 and DDB correlation by real data
+	the fitting for now is only trying the last region
+	there are three input files are needed, one from MC fit result, one from signal ratio histogram, 
+	and the other is the tree from real data
+*/
+
+#define nDDB 20
+#define minDDB 0
+#define maxDDB 1
+
+#define nN2 20
+#define minN2 0
+#define maxN2 0.5
+
+int LookingRegion = 5; // 0 to 5 
+
+template<typename T>
+void setDrawOpt(T& h,string title, string xTitle, string yTitle){
+	h->SetTitle(title.c_str());
+	h->SetTitleSize(0.07);
+	h->GetXaxis()->SetLabelSize(0.05);
+	h->GetXaxis()->SetTitleSize(0.05);
+	h->GetYaxis()->SetLabelSize(0.05);
+	h->GetYaxis()->SetTitleSize(0.05);
+	h->SetXTitle(xTitle.c_str());
+	h->SetYTitle(yTitle.c_str());
+}
+
+Double_t fit_Exp_c(Double_t *x, Double_t* par){
+	//return par[0]*TMath::Exp(par[1]*x[0]);
+	return (TMath::Exp(par[0] + par[1]*x[0]) + par[2] ) * par[3];
+}
+
+vector<float> getResults(TF1* func){
+	int Nparams = 4;
+	cout << "Nparams: " << Nparams << endl;
+	vector<float> params;
+	for (int i=0; i < Nparams; i++){
+		params.push_back(func->GetParameter(i) );
+	}
+	return params;
+}
+
+vector<float> getErrors(TF1* func){
+	int Nparams = 4;
+	vector<float> errors;
+	for(int i=0; i< Nparams; i++){
+		errors.push_back(func->GetParError(i) );
+	}
+	return errors;
+}
+
+float calculateWeight(vector<float> pars, float met){
+	return (TMath::Exp(pars[0] + pars[1]*met) + pars[2] ) * pars[3];
+}
+
+void fit_from_MC(string inputfile){
+	cout << "looking HT range is " << LookingRegion << endl;
+	// setup STLs
+	vector< vector<float> > rFactor;
+	//vector< TH1F* > h_met_mindphi_l(6); // the histo for mindphi > 0.4, 6 is for 6 path trigger regions
+	//vector< TH1F* > h_met_mindphi_s(6); // the histo for mindphi < 0.4
+	TH2F* h_DDB_N2_l = new TH2F("h_DDB_N2_l","",nN2,minN2,maxN2,nDDB,minDDB,maxDDB);
+	setDrawOpt(h_DDB_N2_l,"","N2B1","DDB");
+	TH2F* h_DDB_N2_s = (TH2F*) h_DDB_N2_l->Clone("h_DDB_N2_s");
+
+	TH1F* h_DDB_l = new TH1F("h_DDB_l","",nDDB,minDDB,maxDDB);
+	setDrawOpt(h_DDB_l,"","DDB","");
+	TH1F* h_DDB_s = (TH1F*) h_DDB_l->Clone("h_DDB_s");
+	TH1F* h_N2_l = new TH1F("h_N2_l","",nN2,minN2,maxN2);
+	setDrawOpt(h_N2_l,"","N2B1","");
+	TH1F* h_N2_s = (TH1F*) h_N2_l->Clone("h_N2_s");
+
+	// counter for test //
+	int nWeightGt1 = 0; // try to see the weight number
+	int nWeightLe1 = 0;
+	TH1F* h_weight = new TH1F("h_weight","",10,0,1);
+
+	// load transform vector //	
+	TFile* fin = new TFile("./fit_result.root","READONLY"); // this file name is default
+	//TFile* fin = new TFile("../../fit_result.root","READONLY"); // this file name is default
+	TTreeReader mytree("tree",fin);
+	TTreeReaderArray<float> par(mytree,"par");
+	
+	while(mytree.Next() ){
+		vector<float> tmp;
+		for (auto x : par){
+			tmp.push_back(x);
+		}
+		rFactor.push_back(tmp);
+	}
+	
+	for (auto x : rFactor){
+		for (auto y : x ) {
+			cout << y << " ";
+		}
+		cout << endl;
+	}
+	
+	// fit again to get ratio a as the diff. b/w sig. and MC
+	TF1* func = new TF1("func",fit_Exp_c,0,200,4); // bound min and max, free params
+	cout << "rFactor size: " << rFactor[LookingRegion].size() << endl;
+	for (int i=0; i<rFactor[LookingRegion].size(); i++){
+		func->FixParameter(i,rFactor[LookingRegion][i]);
+	}
+	
+	fin->Close();
+	fin = new TFile("./keep_histo_cutFull.root","READONLY");
+	//fin = new TFile("../../keep_histo_cutFull.root","READONLY");
+	TH1F* h = (TH1F*) fin->Get(Form("h_withSR_%i",LookingRegion) );
+	h->Fit("func","R");
+	Double_t chi2 = func->GetChisquare();
+	vector<float>  params = getResults(func);
+	vector<float>  errors = getErrors(func);	
+	cout << "chisquare : " << chi2 << endl;
+		gStyle->SetOptStat("");
+	// draw the fit result
+	TText* t = new TText(.1*h->GetXaxis()->GetXmax(),.9*h->GetMaximum(),Form("chi2 %f",chi2) );
+	TCanvas* c0 = new TCanvas("c0","c0");
+	h->Draw();
+	t->Draw("SAME");
+	c0->SaveAs("fit_again.png");
+	
+	// input main real data //
+	fin->Close();
+	fin = new TFile(inputfile.data(), "READONLY");
+	TTreeReader data("tree",fin);
+	TTreeReaderArray<bool> cuts(data,"cuts");
+	TTreeReaderValue<float> mindphi(data,"mindphi");
+	TTreeReaderValue<float> metpT(data,"metpT");
+	TTreeReaderArray<float> DDB(data,"DDB");
+	TTreeReaderArray<float> N2B1(data,"N2B1");
+	TTreeReaderValue<int> whichHT(data,"whichHT");
+	int total_entry = data.GetEntries(true);
+	int jEntry = 0;
+	while(data.Next() ){
+		jEntry++;
+		if (jEntry%2 == 0) continue;
+		
+		// apply cuts
+		bool ispass = true;
+		for (int icut = 0; icut < 6; icut++){
+			if (! cuts[icut]){
+				ispass = false;
+				break;
+			}
+		}
+		if (! ispass ) continue;
+		//if (*metpT > 200) continue;
+		if (*whichHT != LookingRegion) continue;
+		if (*mindphi > 0.4) {
+			h_DDB_N2_l->Fill(N2B1[0],DDB[0]);
+			h_DDB_l->Fill(DDB[0]);
+			h_N2_l->Fill(N2B1[0]);
+		} else {
+			float weight = calculateWeight(params,*metpT);
+			//float weight = 1;
+			if (weight > 1 ) nWeightGt1++;
+			else nWeightLe1++;
+			h_DDB_N2_s->Fill(N2B1[0],DDB[0],weight);
+			h_DDB_s->Fill(DDB[0],weight);
+			h_N2_s->Fill(N2B1[0],weight);
+			h_weight->Fill(weight);
+		}
+	}
+
+	TFile* fout = new TFile("QCD_n2b1_realData.root","RECREATE");
+	h_N2_l->Write();
+	h_N2_s->Write();
+	h_DDB_l->Write();
+	h_DDB_s->Write();
+	fout->Close();
+	
+	TCanvas* c1 = new TCanvas("c1","c1");
+	h_DDB_N2_l->Draw("CANDLE");
+	c1->SaveAs("N2B1_DDB_l_withMC.png");
+	h_DDB_N2_s->SetLineColor(kRed);
+	h_DDB_N2_s->Draw("CANDLE SAME");
+	c1->SaveAs("N2B1_DDB_withMC.png");
+	h_DDB_N2_s->Draw("CANDLE");
+	c1->SaveAs("N2B1_DDB_s_withMC.png");
+	cout << "check h_DDB_N2_s " << endl;
+	cout << "nEvent = " << h_DDB_N2_s->Integral()<< endl;
+	cout << " h_DDB_s: " << h_DDB_s->Integral() << endl;
+	cout << " h_N2_s: " << h_N2_s->Integral() << endl;
+	
+	TLegend legend(0.7,0.7,0.9,0.9);
+	legend.AddEntry(h_DDB_l,"mindphi > 0.4");
+	legend.AddEntry(h_DDB_s,"mindphi < 0.4");
+	
+	TCanvas* c2 = new TCanvas("c2","c2");
+	h_DDB_s->Scale(1.0 / (float)h_DDB_s->Integral() );
+	h_DDB_s->SetLineColor(kRed);
+	h_DDB_s->Draw("HISTE");
+	h_DDB_l->Scale(1.0 / (float)h_DDB_l->Integral() );
+	h_DDB_l->Draw("HISTE SAME");
+	legend.Draw("SAME");
+	c2->SaveAs("DDB_both_withMC.png");
+
+	h_N2_s->Scale(1.0 / (float) h_N2_s->Integral() );
+	h_N2_s->SetLineColor(kRed);
+	h_N2_s->Draw("HISTE");
+	h_N2_l->Scale(1.0 / (float) h_N2_l->Integral() );
+	h_N2_l->Draw("HISTE SAME");
+	legend.Draw("SAME");
+	c2->SaveAs("N2_both_withMC.png");
+	
+	// show the static result
+	cout << "nWeightGt1 = " << nWeightGt1 << endl;
+	cout << "nWeightLe1 = " << nWeightLe1 << endl;
+	new TCanvas;
+	h_weight->Draw();
+}
+
+
