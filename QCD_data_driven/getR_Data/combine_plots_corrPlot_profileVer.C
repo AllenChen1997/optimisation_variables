d063f4569031e38b7ecf24b5cefae77238b07e59
Kung-Hsiang Chen <a86792511@gmail.com>
2022-08-03 20:01:10 +0800

    update the code for comparisom plot in getR_data

--- QCD_data_driven/getR_Data/combine_plots_corrPlot_profileVer.C
+++ QCD_data_driven/getR_Data/combine_plots_corrPlot_profileVer.C
@@ -5,19 +5,20 @@
 	limits: two plots with the same binning, same bin width for whole range
 	here h1 is data, h2 is MC
 	target histogram is n2b1 distribution
+	try to use TProfile to draw the plots
 */
 
 using namespace std;
 #define lumi 41000
 
-int lookingHTRegion = 4; 
-// from 1 to 5, [300, 500, 600, 750, 1200, 100000]
+int lookingHTRegion = 4;
+ // from 1 to 5, [300, 500, 600, 750, 1200, 100000]
 string default_input1 = Form("QCD_n2b1_realData.root");
 string default_input2 = "../getR_MC/keep_histo_cutFull.root";
 string default_hist1 = "h_DDB_N2_s";
 string default_hist2 = Form("DDB_N2_%i",lookingHTRegion);
 
-void combine_plots_corrPlot(string inputf1 = default_input1, string inputh1 = default_hist1, string inputf2 = default_input2, string inputh2 = default_hist2){
+void combine_plots_corrPlot_profileVer(string inputf1 = default_input1, string inputh1 = default_hist1, string inputf2 = default_input2, string inputh2 = default_hist2){
 	TFile* fin1 = new TFile(inputf1.data(), "READONLY");
 	TH2F* h1 = (TH2F*) fin1->Get(inputh1.data() );
 	TFile* fin2 = new TFile(inputf2.data(), "READONLY");
@@ -26,31 +27,25 @@ void combine_plots_corrPlot(string inputf1 = default_input1, string inputh1 = de
 	cout << "data integral " << h1->Integral() << endl;
 	cout << "MC integral " << h2->Integral() << endl;
 	
-	h1->SetLineColor(kRed);
-	//h1->Scale(1.0/ h1->Integral());
-	//h2->Scale(lumi);
+	TProfile* hpx1 = h1->ProfileX();
+	TProfile* hpx2 = h2->ProfileX();
+	
 	gStyle->SetOptStat("");
 	TCanvas* c = new TCanvas("c","c");
-	THStack* hs = new THStack("hs","");
-	//h1->Draw("AXIS");
-	hs->Add(h1);
-	hs->Add(h2);
-	hs->Draw("CANDLE SAME");
-	hs->GetXaxis()->SetTitle("N2B1");
-	hs->GetYaxis()->SetTitle("DDB");
-	hs->GetXaxis()->SetBinLabel(1,"");
-	
+	hpx1->SetLineColor(kRed);
+	hpx1->Draw();
+	hpx2->Draw("SAME");
 	TLegend legend(0.8,0.8,1.0,1.0);
 	legend.SetFillColor(0);
 	legend.SetFillStyle(0);
 	legend.SetLineWidth(0);
-	legend.AddEntry(h1,"Data Driven");
-	legend.AddEntry(h2,"MC");
+	legend.AddEntry(hpx1,"Data Driven");
+	legend.AddEntry(hpx2,"MC");
 	legend.Draw("SAME");
 	
-	TGaxis* axis1 = new TGaxis(0,0,0.5,0,0.0,0.5,20,"");
-	axis1->Draw("SAME");
+	// TGaxis* axis1 = new TGaxis(0,0,0.5,0,0.0,0.5,20,"");
+	// axis1->Draw("SAME");
 	
-	c->SaveAs("combine_MC_Data_DDB_N2B1_corr.png");
+	c->SaveAs("combine_MC_Data_DDB_N2B1_corr_profile.png");
 }
 
---
+++ QCD_data_driven/getR_Data/run_compare_plot_code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z $1 ];then
+	echo "use this bash script as following"
+	echo "bash run_compare_plot_code.sh N"
+	echo "N is the maximum number of the HT regions"
+	exit 0
+fi
+Nregion=$1
+for ((i=0; i < $1; i++));do
+	echo "will look at region $i"
+	root -b -q fit_from_MC.C\(\"JetHT-Run2017B-31Mar2018-v1.root\",$i\)
+	sed -i '/lookingHTRegion =/c int lookingHTRegion = '$i';' combine_plots_corrPlot.C
+	root -b -q combine_plots_corrPlot.C
+	sed -i '/lookingHTRegion =/c int lookingHTRegion = '$i';' combine_plots_corrPlot_profileVer.C
+	root -b -q combine_plots_corrPlot_profileVer.C
+	mkdir -p region$i
+	mv combine_MC_Data_DDB_N2B1_corr*.png region$i
+done
